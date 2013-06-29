c8327a3bacc5f6470e0a0bfeacab387268619503
Warren Weckesser <warren.weckesser@gmail.com>
2013-06-29 12:25:44 -0400

    Start work on .travis.yml

--- tests/Make.mm
+++ tests/Make.mm
@@ -6,7 +6,7 @@ PACKAGE = tests
 
 PROJ_CLEAN += $(PROJ_CPPTESTS)
 
-PROJ_PYTESTS = 
+PROJ_PYTESTS = async_map.py test_map.py test_mp.py test_pp.py test_star.py test_with.py
 PROJ_CPPTESTS = 
 PROJ_TESTS = $(PROJ_PYTESTS) # $(PROJ_CPPTESTS)
 PROJ_LIBRARIES = -L$(BLD_LIBDIR) # -lpathos
@@ -17,7 +17,7 @@ PROJ_LIBRARIES = -L$(BLD_LIBDIR) # -lpathos
 all: $(PROJ_TESTS)
 
 test:
-	for test in $(PROJ_TESTS) ; do $${test}; done
+	for test in $(PROJ_PYTESTS) ; do python $${test}; done
 
 release: tidy
 	cvs release .
